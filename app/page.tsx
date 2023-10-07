ecca7069a1db8c285152a6f884d5e24ef2426295
s-techlead <sebastienschnd.techlead@gmail.com>
2023-10-07 19:28:29 +0200

    MainLayout and UploadLayout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+"use client"
 
+import MainLayout from "./layouts/MainLayout"
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <MainLayout>
+        <div>
+          HOME
+        </div>
+      </MainLayout>
+    </>
   )
 }
