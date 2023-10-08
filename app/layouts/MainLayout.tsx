1917f664e02312cbb2372a3bc4298599014952ed
s-techlead <sebastienschnd.techlead@gmail.com>
2023-10-08 10:48:03 +0200

    SideNav

--- app/layouts/MainLayout.tsx
+++ app/layouts/MainLayout.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import SideNavMain from "./includes/SideNavMain"
 import { usePathname } from "next/navigation"
 import TopNav from "./includes/TopNav"
 
@@ -9,9 +10,9 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
       	<>
           <TopNav />
           <div className={`flex justify-between mx-auto w-full lg:px-2.5 px-0 ${pathname == '/' ? 'max-w-[1140px]' : ''}`}>
+            <SideNavMain />
             {children}
           </div>
       	</>
     )
-
 }
