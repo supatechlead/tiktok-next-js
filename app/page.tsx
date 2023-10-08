42cfbad991dae25c0dee14a1ea6d9b3cb005158f
s-techlead <sebastienschnd.techlead@gmail.com>
2023-10-08 11:49:43 +0200

    MainPage | Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
 "use client"
 
 import MainLayout from "./layouts/MainLayout"
+import ClientOnly from "./components/ClientOnly"
+import PostMain from "./components/PostMain"
 
 export default function Home() {
   return (
     <>
       <MainLayout>
-        <div>
-          HOME
+        <div className="mt-[80px]  w-[calc(100%-90px)] max-w-[690px] ml-auto">
+          <ClientOnly>
+            <PostMain post={{
+              id:'123',
+              user_id:'456',
+              video_url: 'beach.mp4',
+              text: 'this is some text',
+              created_at: 'data here',
+              profile: {
+                user_id: '456',
+                name: 'user 1',
+                image: 'https://placehold.co/100'
+              }
+            }}/>
+          </ClientOnly>
         </div>
       </MainLayout>
     </>
