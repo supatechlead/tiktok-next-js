ba746aefd3177b167fb650ad3e869a69875cd028
s-techlead <sebastienschnd.techlead@gmail.com>
2023-10-14 15:19:42 +0200

    Insert environement variables inside AppWriteClient.tsx

--- libs/AppWriteClient.tsx
+++ libs/AppWriteClient.tsx
@@ -1,8 +1,8 @@
 import { Account, Client, ID, Databases, Query, Storage } from 'appwrite';
 
 const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('652a8665d495f7da72c7');
+    .setEndpoint(String(process.env.NEXT_PUBLIC_APPWRITE_URL))
+    .setProject(String(process.env.NEXT_PUBLIC_ENDPOINT))
 
     const account = new Account(client);
     const database = new Databases(client);
